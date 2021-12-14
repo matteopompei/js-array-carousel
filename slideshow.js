e01b2eadc69960247ecc3a1577798a4878d25bfc
Matteo Pompei <datxizzan@gmail.com>
2021-12-14 17:17:40 +0100

    Immagine vetrina in JavaScript

---
+++ slideshow.js
@@ -0,0 +1,10 @@
+let imgArr = [
+  "img/01.jpg",
+  "img/02.jpg",
+  "img/03.jpg",
+  "img/04.jpg",
+  "img/05.jpg"
+]
+
+let imgVetrina = document.getElementById("img-vetrina");
+imgVetrina.src = imgArr[0];
