f489bb0b44ad549506976bc1f975d3b64319971b
Matteo Pompei <datxizzan@gmail.com>
2021-12-14 17:48:39 +0100

    Sostituisco HTML con JavaScript

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,34 @@
-let imgArr = [
-  "img/01.jpg",
-  "img/02.jpg",
-  "img/03.jpg",
-  "img/04.jpg",
-  "img/05.jpg"
+// Definiamo gli Array
+const imgArr = [
+  'img/01.jpg',
+  'img/02.jpg',
+  'img/03.jpg',
+  'img/04.jpg',
+  'img/05.jpg'
+];
+
+const titleArr = [
+  'Svezia',
+  'Svizzera',
+  'Gran Bretagna',
+  'Germania',
+  'Paradise'
 ]
 
+const textArr = [
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+  'Lorem ipsum',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+// Definiamo variabili
 let imgVetrina = document.getElementById("img-vetrina");
 imgVetrina.src = imgArr[0];
+
+let textTitle = document.getElementById("title");
+textTitle.innerHTML = titleArr[0];
+
+let textP = document.getElementById("text");
+textP.innerHTML = textArr[0];
