d82dc096eac152d070f4cf930394604725f1f22b
Matteo Pompei <datxizzan@gmail.com>
2021-12-15 14:54:53 +0100

    Puntatore "active" inserito (non funzionante)

--- slideshow.js
+++ slideshow.js
@@ -13,7 +13,7 @@ const titleArr = [
   'Gran Bretagna',
   'Germania',
   'Paradise'
-]
+];
 
 const textArr = [
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
@@ -21,16 +21,17 @@ const textArr = [
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
   'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+];
 
 // Definiamo variabili
 let mainImage = document.getElementById("slideshow");
 let minImage = document.getElementById("thumbnails");
+let active = 0; // Puntatore
 
 // Immagine in vetrina
 for (let i = 0; i < imgArr.length; i++) {
   mainImage.innerHTML += `
-  <div class="content hidden">
+  <div class="content hidden" id="item-${i}">
     <div class="text">
       <h2>${titleArr[i]}</h2>
       <p>${textArr[i]}</p>
@@ -42,8 +43,14 @@ for (let i = 0; i < imgArr.length; i++) {
 
 // For per le miniature
 for (let i = 0; i < imgArr.length; i++) {
-  minImage.innerHTML += `<img src="${imgArr[i]}" alt="">`;
+  minImage.innerHTML += `<img src="${imgArr[i]}" alt="" id="item-${i}">`;
 }
+
+// Immagini attive
+let mainImageActive = document.getElementById("item-" + active);
+let minImageActive = document.getElementById("item-" + active);
+
+mainImageActive.classList.remove("hidden");
 
 // Frecce
 let prev = document.getElementById("arrow-up");
@@ -53,9 +60,9 @@ prev.addEventListener("click", prevFunc);
 next.addEventListener("click", nextFunc);
 
 function prevFunc() {
-
+  active--;
 }
 
 function nextFunc() {
-
+  active++;
 }
